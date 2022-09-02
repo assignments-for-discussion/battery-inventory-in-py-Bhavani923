47767c4fe2338ab7961dfce4abe9cb67247d0b67
G BHAVANI <77053491+Bhavani923@users.noreply.github.com>
2022-09-02 19:07:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,31 +4,32 @@ def count_batteries_by_usage(cycles):
     mid_count=0
     high_count=0
     counts=dict()
-    #It take batteries one by one and put it in group
-    for x in cycles:
+    
+    for x in cycles:       #It take batteries one by one and put it in group
         if x<=400:
-            low_count+=1   # if the battery has charged less than 400 times then the lowercount is incremented to 1
+            low_count+=1   # if the battery has charged less than or equal to 400 times than the lowercount is incremented to 1
         elif x>400 and x<=919: 
-            mid_count+=1   #similarly  if the battery has charged more than 400 times and less then 919 is incremented to 1
+            mid_count+=1   #similarly  if the battery has charged more than 400 times and less than or equal to 919 is incremented to 1
         else:
-            high_count+=1
+            high_count+=1  #else high count will be incremented
     counts['lowCount']=low_count
     counts['mediumCount']=mid_count
     counts['highCount']=high_count
             
-    return counts
+    return counts          #returning  the values of counts
 
 
 def test_bucketing_by_number_of_cycles():
-  print("Counting batteries by usage cycles...\n");  #Array has been defined based on how many time the batteries are charged
+  print("Counting batteries by usage cycles...\n"); 
+
   #calling to function count_batteries_by_usage()
-  counts = count_batteries_by_usage([100, 300, 500, 600, 900, 1000])
-  #printing lowcount
-  print('lowCount:',counts['lowCount']) 
-  #printing mediumCount
-  print('mediumCount',counts['mediumCount'])
-  #printing highCount
-  print('highCount:',counts['highCount'])
+  counts = count_batteries_by_usage([100, 300, 500, 600, 900, 1000])  #Array has been defined based on how many time the batteries are charged
+  
+  print('lowCount:',counts['lowCount'])             #prints the number of batteries come under lowercount
+  
+  print('mediumCount',counts['mediumCount'])        #prints the number of batteries come under mediumCount             
+ 
+  print('highCount:',counts['highCount'])           #prints the number of batteries come under highCount
   print("Done counting :)")
 
 
